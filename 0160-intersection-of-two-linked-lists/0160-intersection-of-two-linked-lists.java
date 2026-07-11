12b4a0c5f2f44320c0376659d9ada3ef66349bc6
Shivam-afk-bit <patilshivam874@gmail.com>
2026-07-11 14:43:01 +0530

    Time: 1 ms (99.93%), Space: 52.6 MB (79.76%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,19 +12,56 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode temp1 = headA;
-        ListNode temp2 ;
+        ListNode temp2 = headB;
+        ListNode temp;
+        int length1 = 1;
+        int length2 = 1;
 
-        while(temp1 != null){
-            temp2 = headB;
-            while(temp2 != null){
-                if(temp1 == temp2){
-                    return temp1;
-                }
-                temp2 = temp2.next;
-            }
-            temp1 = temp1.next;
+
+        while(temp1.next != null){
+            length1++;
+            temp1 =  temp1.next;
         }
 
-        return null;
-    }
+        while(temp2.next != null){
+            length2++;
+            temp2 = temp2.next;
+        }
+
+
+        int difference = Math.abs(length1 - length2); 
+      
+        
+        if(length1 > length2){
+                temp1 = headA;
+                temp2 = headB;
+                while(difference != 0){
+                    temp1 = temp1.next;
+                    difference--;
+                 }
+                }else if(length1 < length2){
+                    temp2 = headB;
+                    temp1 =  headA;
+                    while(difference !=0){
+                        temp2 =  temp2.next;
+                        difference--;
+                    }
+                }else {
+                    temp1 = headA;
+                    temp2 = headB;
+                }
+
+
+                while(temp1 != null){
+                    if(temp1 == temp2){
+                        return temp1;
+                    }
+                    temp1 = temp1.next;
+                    temp2 = temp2.next;
+                }
+                return null;
+    
+        }
+
+       
 }
