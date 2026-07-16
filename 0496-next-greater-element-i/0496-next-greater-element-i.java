dee05909cf0262a6034701741e52164a43c50536
Shivam-afk-bit <patilshivam874@gmail.com>
2026-07-16 12:56:59 +0530

    Time: 4 ms (74.7%), Space: 45.3 MB (49.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        int ans[] = new int[nums1.length];
-        for(int i=0; i< nums1.length; i++){
-            for(int j=0; j< nums2.length;j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j+1; k<= nums2.length; k++){
-                        if(k == nums2.length){
-                            ans[i] = -1;
-                            break;
-                        }else if(nums2[k] > nums2[j]){
-                            ans[i] = nums2[k];
-                            break;
-                        }else{
-                            ans[i] = -1;
-                        }
-                    }
-                }
+       Stack<Integer> stack = new Stack<>();
+       HashMap<Integer,Integer> map = new  HashMap<>();
+       int[] ans = new int[nums1.length];
+
+       for(int i = nums2.length-1; i >= 0 ; i--){
+        if(stack.isEmpty()){
+            map.put(nums2[i] , -1);
+            stack.push(nums2[i]);
+        }else if(nums2[i] > stack.peek()){
+            while( !stack.isEmpty() && nums2[i] > stack.peek()){
+                stack.pop();
             }
-        }
-        return ans;
+            if(stack.isEmpty()){
+                map.put(nums2[i] , -1);
+                stack.push(nums2[i]);
+            }else{
+                map.put(nums2[i] , stack.peek());
+                stack.push(nums2[i]);
+            }
+            
+            
+            
+         }else{
+            map.put(nums2[i] , stack.peek());
+            stack.push(nums2[i]);
+           
+         }
+       }
+
+       for(int i = 0; i < nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
+       }
+
+       return ans;
     }
 }
