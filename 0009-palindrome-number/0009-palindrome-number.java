b74433d382433d51771e9f412c71b42e30657c7d
Shivam-afk-bit <patilshivam874@gmail.com>
2026-08-05 18:19:25 +0530

    Time: 6 ms (21.95%), Space: 46 MB (33.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 ){
+            return false;
+        }
+
+        int n = x;
+        int rev = 0;
+
+
+        while(x > 0){
+            int z = x % 10;
+
+            rev = rev*10 + z;
+
+            x = x/10;
+        }
+
+        if(n == rev){
+            return true;
+        }
+
+        return false;
+
+    }
+}
